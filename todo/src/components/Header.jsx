1e778224163306a9504f3915cb78fcac8a0f68d2
Joseph <jotienoochieng@gmail.com>
2025-03-29 06:45:05 +0300

    first commit

---
+++ todo/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <svg>
+        <path d="" />
+      </svg>
+      <h1>TODO</h1>
+    </div>
+  );
+};
+
+export default Header;
